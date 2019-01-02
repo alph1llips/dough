96c3d67454a441f66a97be3ac193d719e8a82582
alph1llips <alphillips1030@gmail.com>
2019-01-01 22:56:01 -0600

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+/*order matters
+* static
+* empty
+* wilcard
+* */
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent, data: {title: 'Hello World'}},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( appRoutes, {enableTracing: true})
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HomeComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
